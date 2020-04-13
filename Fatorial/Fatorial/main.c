2ed69cd16d28cb179b2109d24e2ba3d3dc422c9f
alinesc <aline.revisoes@gmail.com>
2020-04-13 18:04:09 -0300

    Exercício fatorial feito

--- Fatorial/Fatorial/main.c
+++ Fatorial/Fatorial/main.c
@@ -3,6 +3,24 @@
 
 #include <stdio.h>
 
+int fatorial (int x) {
+    int calculo = 1;
+    int c;
+    for (c = 2; c <= x; c++){
+        calculo = calculo * c;
+    }
+    return calculo;
+}
+
 int main(int argc, const char * argv[]) {
+    int n_elementos;
+    printf("Digite o número de elementos: ");
+    scanf("%i", &n_elementos);
+    int tam_grupo;
+    printf("Digite o número de elementos: ");
+    scanf("%i", &tam_grupo);
+    int a;
+    a = fatorial(n_elementos) / fatorial(n_elementos - tam_grupo);
+    printf("O resultado é %i.", a);
     return 0;
 }
