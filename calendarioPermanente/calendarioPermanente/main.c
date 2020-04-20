5c725cd3e59563e8ec601eecb5b776a1ed2c2cc5
alinesc <aline.revisoes@gmail.com>
2020-04-20 18:35:04 -0300

    Calendário permanente desenvolvido

--- calendarioPermanente/calendarioPermanente/main.c
+++ calendarioPermanente/calendarioPermanente/main.c
@@ -5,9 +5,17 @@
 #include <locale.h>
 
 int procurarLinha(int L, int C, int mat[L][C], int proc) {
-    
-    return proc;
+    int lin, col, n;
+    for (lin=0; lin <= L; lin++){
+        for(col=0; col <= C; col++){
+            if (mat[lin][col] == proc) {
+                n = lin;
+            }
+        }
+    }
+    return n;
 }
+
 int A1[28][4], A2[28][4];
 int B[28][12] =
 {{4,0,0,3,5,1,3,6,2,4,0,2},
@@ -39,7 +47,9 @@ int B[28][12] =
 {1,4,4,0,2,5,0,3,6,1,4,6},
 {2,5,6,2,4,0,2,5,1,3,6,1}};
 int C[7][6];
+
 int main(int argc, const char * argv[]) {
+
     // populando a tabela A1
     int l, c, x= -3;
     for(c=0; c<4; c++){
@@ -60,10 +70,10 @@ int main(int argc, const char * argv[]) {
     
     // populando a tabela A2
     
-    x = -15;
+    x = -19;
     for(c=0; c<4; c++){
-        for(l=0; l<28; l++, c++) {
-            if (x<1) {
+        for(l=0; l<28; l++, x++) {
+            if (x < 1) {
                 A2[l][c] = -1;
             }
             else {
@@ -95,8 +105,38 @@ int main(int argc, const char * argv[]) {
     }
     if (ano < 2001) {
         ano = ano % 100;
+        linha = procurarLinha(28, 4, A1, ano);
     }
-        
-        //ano = ano % 100;
+    else {
+        ano = ano % 100;
+        linha = procurarLinha(28, 4, A2, ano);
+    }
+    resposta = (B[linha][mes-1]) + dia;
+    linha = procurarLinha(7, 6, C, resposta);
+    switch (linha) {
+        case 0:
+            printf("Domingo\n");
+            break;
+        case 1:
+            printf("Segunda\n");
+            break;
+        case 2:
+            printf("Terça\n");
+            break;
+        case 3:
+            printf("Quarta\n");
+            break;
+        case 4:
+            printf("Quinta\n");
+            break;
+        case 5:
+            printf("Sexta\n");
+            break;
+        case 6:
+            printf("Sábado\n");
+            break;
+        default:
+            break;
+    }
     return 0;
 }
