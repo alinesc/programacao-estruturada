4992f800154c8456c1cd691bbbc9b4d1ae6c0af7
alinesc <aline.revisoes@gmail.com>
2020-06-06 17:10:25 -0300

    Criação da função Update

--- agendaDeAmigos/agendaDeAmigos/main.c
+++ agendaDeAmigos/agendaDeAmigos/main.c
@@ -36,13 +36,14 @@ int main(int argc, const char * argv[]) {
     char opcao[10] = "0";
     setlocale(LC_ALL, "Portuguese");
     
-    while(opcao[0] != '5') {
+    while(opcao[0] != '6') {
         printf("\nEntre com a opção desejada: ");
         printf("\n1) Inserir ficha: ");
         printf("\n2) Procurar por nome: ");
         printf("\n3) Listar toda a base: ");
         printf("\n4) Excluir por nome: ");
-        printf("\n5) Sair\n\n ");
+        printf("\n5) Alterar registro (busca por nome) : ");
+        printf("\n6) Sair\n\n ");
         fgets(opcao, 10, stdin);
         
         if (opcao[0] == '1') {
@@ -105,6 +106,65 @@ int main(int argc, const char * argv[]) {
                 proxima --;
             }
         }
+        
+        if (opcao[0] == '5') {
+            char procurado[40];
+            printf("\nEntre com o nome que será alterado: ");
+            fgets(procurado, 40, stdin);
+            strtok(procurado, "\n");
+            
+            int indice = procurarRecursivo(procurado, amigos, fichas_existentes, 0);
+            if (indice == -1) {
+                printf("\nNome não encontrado. ");
+            }
+            else {
+                int menu;
+                do {
+                    printf("Qual campo você quer alterar?\n");
+                    printf("Digite 1 - nome \n 2 - telefone \n 3 - endereço \n 4 - email \n 5 - idade \n 6 - rede social \n 7 - sair \n");
+                    scanf("%i", &menu);
+                    getchar();
+                switch (menu) {
+                    case 1:
+                        printf("\nEntre com um nome: ");
+                        fgets(amigos[indice].nome, 40, stdin);
+                        strtok(amigos[indice].nome, "\n");
+                        break;
+                    case 2:
+                        printf("\nEntre com um telefone: ");
+                        fgets(amigos[indice].telefone, 40, stdin);
+                        break;
+                    case 3:
+                        printf("\nEntre com um endereco: ");
+                        fgets(amigos[indice].endereco, 100, stdin);
+                        break;
+                    case 4:
+                        printf("\nEntre com um email: ");
+                        fgets(amigos[indice].email, 50, stdin);
+                        break;
+                    case 5:
+                        printf("\nEntre com a idade: ");
+                        scanf("%d", &amigos[indice].idade);
+                        getchar();
+                        break;
+                    case 6:
+                        printf("\nEntre com um @ do Instagram: ");
+                        fgets(amigos[indice].redeSocial, 50, stdin);
+                        break;
+                    case 7:
+                        break;
+                    default:
+                        printf("Opção inválida, tente novamente");
+                        break;
+                }
+                }
+                while (menu < 7);
+                printf("\n\nO telefone de %s, %s, endereco %s, email %s, idade %d e Instagram %s, foi alterado \n", amigos[indice].nome, amigos[indice].telefone, amigos[indice].endereco, amigos[indice].email, amigos[indice].idade, amigos[indice].redeSocial );
+            }
+        }
+        
+        
+        
     }
     return 0;
     }
